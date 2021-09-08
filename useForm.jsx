867e69d74143c57585ec3c15ed2885d3d7991077
José Castro <hairam.jc@gmail.com>
2021-09-08 17:31:00 -0300

    Primer commit

---
+++ useForm.jsx
@@ -0,0 +1,22 @@
+import { useState } from "react"
+
+
+const useForm = (initialState = {}) => {
+    const [values, setValues] = useState(initialState);
+
+    const reset = () => {
+        setValues(initialState);
+    }
+
+    const handleInputChange = ({target}) => {
+        //console.log(e.target.value);
+        setValues({
+            ...values,
+            [target.name] : target.value
+        })
+    }
+
+    return [ values, handleInputChange, reset ];
+}
+
+export default useForm
